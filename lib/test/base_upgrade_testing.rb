d3aec8bed44cd94e9f95951ccac561954e76609d
thomashaw <thomashaw@gmail.com>
2018-10-01 21:13:25 +0100

    Automatically add generated scenarios to SecGen batch.

--- lib/test/base_upgrade_testing.rb
+++ lib/test/base_upgrade_testing.rb
@@ -3,6 +3,8 @@
 #  Script for generating test scenarios for identifying conflicts and
 #  version mismatches within a recently upgraded base box.
 #
+#  Requires: SecGen batch database configured, see README-Batch-VMs.md
+#
 #  1) A scenario file is created for each software module within lib/test/tmp/
 #  2) a batch_secgen entry is created for each generated scenario
 #  3) batch_secgen spins up each scenario and reports on failures
@@ -18,6 +20,7 @@ require_relative '../output/xml_scenario_generator.rb'
 require_relative '../objects/system'
 require_relative '../objects/module'
 
+@path_to_ruby = '/home/thomashaw/.rvm/rubies/ruby-2.3.4/bin/ruby'
 
 def select_base
   bases = ModuleReader.read_bases
@@ -64,12 +67,13 @@ def generate_scenarios(selected_base)
 
   available_software_modules = vulnerabilities + services + utilities
 
-
   # If the module conflicts with the base, remove it.
-  available_software_modules.delete_if {|module_for_possible_exclusion|
+  available_software_modules.delete_if do |module_for_possible_exclusion|
     (module_for_possible_exclusion.conflicts_with(selected_base) ||
         selected_base.conflicts_with(module_for_possible_exclusion))
-  }
+  end
+
+  output_scenario_paths = []
 
   available_software_modules.each do |mod|
     ## Create a system_name based on the selected module and the base name
@@ -96,13 +100,24 @@ def generate_scenarios(selected_base)
       File.open(output_filename, 'w+') do |file|
         file.write(xml_content)
       end
+      output_scenario_paths << "#{tmp_dir}/#{output_filename}"
     rescue StandardError => e
       Print.err "Error writing file: #{e.message}"
       abort
     end
   end
+  output_scenario_paths
+end
+
+def add_to_secgen_batch(scenario_paths)
+  Print.info "Adding #{scenario_paths.size} jobs to batch queue"
+   scenario_paths.each do |scenario_path|
+      puts `#{@path_to_ruby} #{ROOT_DIR}/lib/batch/batch_secgen.rb add --instances 1 --- -s #{scenario_path} --read-options secgen.conf r`
+   end
 end
 
 Print.info 'Script for generating base module upgrade test scenarios'
 base = select_base
-generate_scenarios(base)
+output_scenario_paths = generate_scenarios(base)
+Print.info 'Scenario files generated successfully.'
+add_to_secgen_batch(output_scenario_paths)
